5e54a16dc2acb46d8889bb1ad5f2c981f71a3774
Mateus de Castro <samuraiexx@gmail.com>
2020-10-22 02:20:28 -0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/interactive_markov_chain/precache-manifest.28aef43a959c1946f8a5cb8990d13c40.js"
+  "/interactive_markov_chain/precache-manifest.0da7d9f20329850f3e9e8ac6b2106469.js"
 );
 
 self.addEventListener('message', (event) => {
